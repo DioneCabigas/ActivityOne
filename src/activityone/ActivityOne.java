41c128f79568cc01cfa701b661113319c3bd6a6b
Dione Alfred Cabigas <dionealfred.cabigas@cit.edu>
2023-09-25 19:40:46 +0800

    First

--- src/activityone/ActivityOne.java
+++ src/activityone/ActivityOne.java
@@ -1,2 +1,7 @@
-package activityone;public class ActivityOne {
+package activityone;
+
+public class ActivityOne {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
